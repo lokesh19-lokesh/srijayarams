aec875733c09b0a6fac5a58c1aa86fdae2f5442c
lokesh19-lokesh <lokeshmaddibiona@gmail.com>
2025-11-24 19:58:29 +0530

    dfsd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
       <div className="container mx-auto px-4 md:px-8 flex justify-between items-center">
         {/* Logo */}
         <Link to="/" className="flex items-center">
-          <img src="/src/assets/logo.png" alt="Sri Jayarama Group" className="h-16 w-auto" />
+          <img src="assets/logo.png" alt="Sri Jayarama Group" className="h-16 w-auto" />
         </Link>
 
         {/* Desktop Navigation */}
