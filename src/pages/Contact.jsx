1cc1f0ce69f3df5e798b3bd407f8f12413b36bb1
lokesh19-lokesh <lokeshmaddibiona@gmail.com>
2025-11-25 10:19:10 +0530

    bsd

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -69,6 +69,23 @@ const Contact = () => {
           </div>
         </div>
       </Section>
+
+      {/* Google Map */}
+      <Section className="py-0 pb-24">
+        <div className="w-full h-[450px] rounded-2xl overflow-hidden border border-gray-800 shadow-2xl">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d924.0326803656382!2d78.48884747031312!3d17.53443601943427!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb854c2ca54d83%3A0x4d1b14acc1c9c176!2sSri%20Jayarama%20Automotives%20Pvt%20Ltd!5e0!3m2!1sen!2sin!4v1764045734311!5m2!1sen!2sin"
+            width="100%"
+            height="100%"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            title="Sri Jayarama Automotives Location"
+            className="grayscale hover:grayscale-0 transition-all duration-500"
+          ></iframe>
+        </div>
+      </Section>
     </>
   );
 };
